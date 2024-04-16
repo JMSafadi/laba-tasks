bac835ba8e2c2c0eafb3015596e226a7eb06ffad
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-04-16 12:15:56 -0300

    task-3-pending-approval

--- 3/functions.js
+++ 3/functions.js
@@ -1,29 +1,29 @@
 // Task 1
 const products = [136, 64, 13409, 7]
-// console.log('Initial array:', products)
 
 function calculateDiscountedPrice(array, discount) {
   const productsDiscounted = array.map(prod => prod - (prod * discount / 100))
   return productsDiscounted
 }
-console.log('Discounted array:', calculateDiscountedPrice(products, 10))
+console.log(calculateDiscountedPrice(products, 10))
 
 function calculateTotalPrice(arr) {
-  const totalProductsPrice = arr.reduce((acc, value) => acc + value)
+  const totalProductsPrice = arr.reduce((acc, value) => acc + value, 0)
   return totalProductsPrice
 }
-// console.log('Products total price:', calculateTotalPrice(products))
+console.log(calculateTotalPrice(products))
 
 
 // Task 2
 const compose = (...functions) => arg => functions.reduceRight((accum, fn) => fn(accum), arg)
 
 // 
+const getFullName = person => `${person.firstName} ${person.lastName}`
+
 const person1 = {
   firstName: 'Julian',
   lastName: 'Safadi'
 }
-const getFullName = person => `${person.firstName} ${person.lastName}`
 console.log(getFullName(person1))
 
 // 
@@ -41,6 +41,14 @@ const filterUniqueWords = compose(sortWords, filterUnique)
 console.log(filterUniqueWords('this this is an example for task task 3'))
 
 // 
+const filterGrades = (arr) => arr.map(student => student.grade)
+const addGrades = (arr) => {
+  const total = arr.reduce((acc, value) => acc + value, 0)
+  return { total , arr }
+}
+const averageGrades = ({ total, arr }) => total / arr.length
+const getAverageGrade = compose(averageGrades, addGrades, filterGrades)
+
 const students = [{
   name: 'Gonzalo',
   grade: 3
@@ -52,14 +60,7 @@ const students = [{
   grade: 2
 }]
 
-const filterGrades = (arr) => arr.map(student => student.grade)
-const addGrades = (arr) => {
-  const total = arr.reduce((acc, value) => acc + value, 0)
-  return { total , arr }
-}
-const averageGrades = ({ total, arr }) => total / arr.length
-const getAverageGrade = compose(averageGrades, addGrades, filterGrades)
-console.log(getAverageGrade(students))
+console.log('average', getAverageGrade(students))
 
 
 // Task 3
@@ -68,25 +69,27 @@ const createCounter = function() {
   return () => ++count
 }
 
+
 const counter1 = createCounter()
 const counter2 = createCounter()
 
 console.log(counter1())
 console.log(counter1())
 console.log(counter1())
+
 console.log(counter2())
 console.log(counter2())
 console.log(counter2())
 
 function repeatFunction(fn, num) {
   if (num < 0) {
-    return function() {
+    return () => {
       while (true) {
         fn()
       }
     }
   } else {
-    return function() {
+    return () => {
       for (i = 0; i < num; i++) {
         fn()
       }
@@ -94,9 +97,75 @@ function repeatFunction(fn, num) {
   }
 }
 
-const sayHello = () => {
-  console.log('Hello')
-}
-
-const repeat = repeatFunction(sayHello, 3)
+const sayHi = () => console.log('hi')
+const repeat = repeatFunction(sayHi, 3)
 console.log(repeat())
+
+// Task 4
+function calculateFactorial(num) {
+  if (num <= 1) return 1
+  return num * calculateFactorial(num - 1)
+}
+console.log(calculateFactorial(5))
+
+// 
+function power(base, exp) {
+  if (exp === 0) {
+    return 1
+  } else if (exp < 0) {
+    return 1 / power(base, -exp)
+  } else {
+    return base * power(base, exp - 1)
+  }
+}
+console.log(power(2, 3))
+
+// Task 5 
+function lazyMap(arr, mapFn) {
+  let index = 0
+  return {
+    next: () => {
+      if (index < arr.length) {
+        const result = mapFn(arr[index])
+        index++
+        return { value: result, done: false }
+      } else {
+        return { done: true }
+      }
+    }
+  }
+}
+const numbers = [5, 10, 30]
+const mapFn = x => x * 2
+const lazyGenerator = lazyMap(numbers, mapFn)
+
+console.log(lazyGenerator.next().value)
+console.log(lazyGenerator.next().value)
+console.log(lazyGenerator.next().value)
+
+// 
+function fibonacciGenerator() {
+  let current = 0
+  let next = 1
+  return {
+    next: () => {
+      let result = current
+      let temp = next
+      next = current + next
+      current = temp
+      debugger
+      return result
+    }
+  }
+
+}
+const fibonacci = fibonacciGenerator()
+
+console.log(fibonacci.next())
+console.log(fibonacci.next())
+console.log(fibonacci.next())
+console.log(fibonacci.next())
+console.log(fibonacci.next())
+console.log(fibonacci.next())
+console.log(fibonacci.next())
+console.log(fibonacci.next())
