8c8e4020bf5e356028ee05e7c9671abc0abf5b96
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-04-24 00:04:57 -0300

    task-5-pending-review

--- 5/arraysManipulation.js
+++ 5/arraysManipulation.js
@@ -104,3 +104,49 @@ function getArrayUnion(arr1, arr2) {
   return Array.from(joined)
 }
 console.table(getArrayUnion(nums1, nums2)) // [6, 65, 31, 20, 15, 3, 40, 11]
+
+// 5
+function measureArrayPerformance(arr, fn) {
+  const startExecution = performance.now()
+  fn(arr)
+  const endExecution = performance.now()
+  return (endExecution - startExecution).toFixed(3)
+}
+
+function customReduce(arr) {
+  let reduce = 0
+  for (let i = 0; i < arr.length; i++) {
+    reduce += arr[i]
+  }
+  return reduce
+}
+
+// Comparing execution times
+// Reduce
+const customReduceTime = measureArrayPerformance(nums1, arr => customReduce(arr))
+const builtInReduceTime = measureArrayPerformance(nums1, arr => arr.reduce((acc, value) => acc + value, 0))
+
+if (customReduceTime < builtInReduceTime) {
+  console.log(`My custom reduce function was faster than built-in method with ${customReduceTime} miliseconds`)
+} else {
+  console.log(`Built-in reduce method was faster than my custom function with ${builtInReduceTime} miliseconds`)
+}
+
+// Map
+function customMap(arr) {
+  const newArray = []
+  for (let i = 0; i < arr.length; i++) {
+    const randomNum = parseInt((Math.random() * 100).toFixed(0))
+    newArray.push(randomNum)
+  }
+  return newArray
+}
+
+const customMapTime = measureArrayPerformance(nums2, arr => customMap(arr))
+const builtInMapTime = measureArrayPerformance(nums2, arr => arr.map(() => parseInt((Math.random() * 100).toFixed(0))))
+
+if (customMapTime < builtInMapTime) {
+  console.log(`My custom map function was faster than built-in method with ${customMapTime} miliseconds`)
+} else {
+  console.log(`Built-in map method was faster than my custom function with ${builtInMapTime} miliseconds`)
+}
